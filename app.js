c8494fa3ae63f5f96bc8cecfe52b15c18585bc13
Subash Banstola <subashbanstola11@gmail.com>
2023-10-03 16:31:02 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("Welcome to NodeJS")
+const a = 10;
+const b = 20
+console.log(a)
+console.log(b)
+const sum = a + b
+console.log(sum)
+
+//array
+const numbers = [10,20,30]
+console.log(numbers[1])
+console.log(numbers[0])
+
+//to calculate length of array
+console.log(numbers.length)
+
+//objects
+const person= {
+    name: "Subash",
+    age: 21
+}
+//{key:value}
+console.log("My name is " + person.name)
+console.log(person.age)
